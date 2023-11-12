74ddc89dd2575e1e14405382cc8b6b2678ed9f1d
Weslen Lakins <weslen.lakins@outlook.com>
2023-11-11 21:02:55 -0500

    added listing item component

---
+++ src/pages/Category.jsx
@@ -0,0 +1,95 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from 'firebase/firestore'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import Spinner from '../components/Spinner'
+import ListingItem from '../components/ListingItem'
+
+function Category() {
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const params = useParams()
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        // Get reference to listings collection
+        const listingsRef = collection(db, 'listings')
+
+        // Create a query based on category
+        const q = query(
+          listingsRef,
+          where('type', '==', params.categoryName),
+          orderBy('timestamp', 'desc'),
+          limit(10)
+        )
+
+        // Execute query
+        const querySnap = await getDocs(q)
+
+        // Create empty array to store listings
+        const listings = []
+
+        // Loop through query results
+        querySnap.forEach((doc) => {
+          // Push each listing to listings array
+          return listings.push({
+            id: doc.id,
+            data: doc.data(),
+          })
+        })
+
+        // Set listings state to listings array and loading state to false
+        setListings(listings)
+        setLoading(false)
+      } catch (error) {
+        toast.error('Could not fetch listings')
+      }
+    }
+
+    fetchListings()
+  }, [params.categoryName])
+
+  return (
+    <div className='category'>
+      <header>
+        <p className='pageHeader'>
+          {params.categoryName === 'rent'
+            ? 'Places for rent'
+            : 'Places for sale'}
+        </p>
+      </header>
+      {loading ? (
+        <Spinner />
+      ) : listings && listings.length > 0 ? (
+        <>
+          <main>
+            <ul className='categoryListings'>
+              {listings.map((listing) => (
+                <ListingItem
+                  listing={listing.data}
+                  id={listing.id}
+                  key={listing.id}
+                />
+              ))}
+            </ul>
+          </main>
+        </>
+      ) : (
+        <p>No listings for {params.categoryName}</p>
+      )}
+    </div>
+  )
+}
+
+export default Category
