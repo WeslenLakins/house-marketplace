d2e7f7e9e42e933524d619bdcefdea7b2109607b
Weslen Lakins <weslen.lakins@outlook.com>
2023-11-14 13:20:16 -0500

    added listing details, contact, and leaflet map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
@@ -35,8 +36,6 @@ function Listing() {
 
   return (
     <main>
-      {/* SLIDER */}
-
       <div
         className='shareIconDiv'
         onClick={() => {
@@ -90,11 +89,27 @@ function Listing() {
 
         <p className='listingLocationTitle'>Location</p>
 
-        {/* MAP */}
+        <div className='leafletContainer'>
+          <MapContainer
+            style={{ height: '100%', width: '100%' }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            />
+
+            <Marker
+              position={[listing.geolocation.lat, listing.geolocation.lng]}>
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
-            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            to={`/contact/${listing.userRef}?listingName=${listing.name}=${listing.location}`}
             className='primaryButton'>
             Contact Landlord
           </Link>
@@ -105,3 +120,5 @@ function Listing() {
 }
 
 export default Listing
+
+// https://stackoverflow.com/questions/67552020/how-to-fix-error-failed-to-compile-node-modules-react-leaflet-core-esm-pat
